33fed88539a060ad873c642c480cdc2707ecf3e0
rafisgithub <rafi.cse.ahmed@gmail.com>
2025-08-16 12:37:02 +0600

    fix: Rename and update seeding functions for system settings and social media

--- apps/seeders/management/commands/seed.py
+++ apps/seeders/management/commands/seed.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from apps.system_setting.seed_system_setting import seed_about_system, seed_social_media, seed_smtp_credentials
+from apps.system_setting.seed_system_setting import seed_system_setting, seed_social_media, seed_smtp_credentials
 from apps.users.seed_users import seed_users
 
 
@@ -8,7 +8,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         seed_users()
-        seed_about_system()
+        seed_system_setting()
         seed_social_media()
         seed_smtp_credentials()
     
