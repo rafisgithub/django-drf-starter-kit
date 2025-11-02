0bee71cbf28cef23a7e802991de72b35a073d2de
rafisgithub <rafi.cse.ahmed@gmail.com>
2025-11-02 11:56:45 +0600

    Update database engine to PostgreSQL for production settings

--- project/settings.py
+++ project/settings.py
@@ -137,16 +137,19 @@ MASTER_USER_EMAIL = "rafi.cse.ahmed@gmail.com"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 if DEBUG:
+
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
+    
 else:
+
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.mysql",
+            "ENGINE": "django.db.backends.postgresql",
             "NAME": "your_database_name",
             "USER": "your_database_user",
             "PASSWORD": "your_database_password",
