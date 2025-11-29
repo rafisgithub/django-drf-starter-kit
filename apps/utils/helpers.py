fac53075dd57e9920532a8bf52d547d726113fe9
rafisgithub <rafi.cse.ahmed@gmail.com>
2025-11-29 17:10:23 +0600

    Rename parameter 'code' to 'status_code' in success and error response functions for clarity

--- apps/utils/helpers.py
+++ apps/utils/helpers.py
@@ -5,19 +5,19 @@ from rest_framework import status
 from django.core.mail import EmailMultiAlternatives
 
 
-def success(data=None, message="Success", code=status.HTTP_200_OK):
+def success(data=None, message="Success", status_code=status.HTTP_200_OK):
     return Response({
         "success": True,
         "message": message,
         "data": data
-    }, status=code)
+    }, status=status_code)
 
-def error(message="Error", errors=None, code=status.HTTP_400_BAD_REQUEST):
+def error(message="Error", errors=None, status_code=status.HTTP_400_BAD_REQUEST):
     return Response({
         "success": False,
         "message": message,
         "errors": errors
-    }, status=code)
+    }, status=status_code)
 
 
 
